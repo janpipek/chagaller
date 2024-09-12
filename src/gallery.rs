d646e8aba3d511b54e8a59f314e3206117511641
Jan Pipek <jan.pipek@gmail.com>
2024-09-12 16:51:55 +0200

    Simple resize to 1600x1600

--- src/gallery.rs
+++ src/gallery.rs
@@ -1,5 +1,12 @@
+use image::imageops::FilterType;
+use image::ImageReader;
 use std::fs;
 use std::path::PathBuf;
+
+pub struct GalleryOpts {
+    pub max_width: u32,
+    pub max_height: u32,
+}
 
 pub fn find_image_files(input_dir: &PathBuf) -> Vec<PathBuf> {
     let allowed_extensions = vec!["jpg", "jpeg"];
@@ -15,20 +22,30 @@ pub fn find_image_files(input_dir: &PathBuf) -> Vec<PathBuf> {
     return result;
 }
 
-pub fn create_gallery(output_dir: &PathBuf, source_images: &Vec<PathBuf>) {
+pub fn create_gallery(output_dir: &PathBuf, source_images: &Vec<PathBuf>, opts: &GalleryOpts) {
     if output_dir.exists() && !output_dir.is_dir() {
         return;
     }
 
     fs::create_dir_all(output_dir).ok();
+    fs::create_dir(output_dir.join("images")).ok();
+    fs::create_dir(output_dir.join("thumbnails")).ok();
 
     for image_path in source_images {
-        render_image(image_path, output_dir);
+        process_image(image_path, output_dir, opts);
     }
+
+    // create_gallery_page()
 }
 
-pub fn render_image(image_path: &PathBuf, output_dir: &PathBuf) {
+pub fn process_image(image_path: &PathBuf, output_dir: &PathBuf, opts: &GalleryOpts) {
     let name = image_path.file_name().unwrap();
-    let target_path = output_dir.join(name);
-    fs::copy(image_path, target_path);
+    let mut target_path = output_dir.join("images").join(name);
+    target_path.set_extension("jpg");
+
+    let img = ImageReader::open(image_path).unwrap().decode().unwrap();
+    let scaled_img = img.resize(opts.max_width, opts.max_height, FilterType::Nearest);
+    scaled_img.save(&target_path).ok();
+
+    // fs::copy(image_path, target_path);
 }
--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,19 @@ use std::path::PathBuf;
 use crate::cli::{Cli, Commands};
 use crate::gallery::{create_gallery, find_image_files};
 use clap::Parser;
+use gallery::GalleryOpts;
 
 fn process(input_dir: &PathBuf, output_dir: &PathBuf) {
     let dir_str = input_dir.display();
     println!("Processing '{dir_str}'.");
 
-    let image_files = find_image_files(input_dir);
-    for f in &image_files {
-        println!("{}", f.display());
-    }
+    let opts = GalleryOpts {
+        max_width: 1600,
+        max_height: 1600,
+    };
 
-    create_gallery(output_dir, &image_files);
+    let image_files = find_image_files(input_dir);
+    create_gallery(output_dir, &image_files, &opts);
 }
 
 fn main() {
