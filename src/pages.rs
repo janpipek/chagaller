e8b39b9c13535834c3c5192a10bfe5fc82c8dfeb
Jan Pipek <jan.pipek@gmail.com>
2024-09-13 09:23:22 +0200

    index.html

--- src/pages.rs
+++ src/pages.rs
@@ -1,5 +1,6 @@
 use askama::Template;
 use crate::gallery::{Gallery, GalleryOpts};
+use crate::image::Image;
 
 #[derive(Template)]
 #[template(path = "index.html")]
@@ -7,4 +8,18 @@ pub struct IndexTemplate<'a> {
     pub gallery: &'a Gallery,
     pub gallery_opts: &'a GalleryOpts,
     // pub title: &'a str,
+}
+
+
+#[derive(Template)]
+#[template(path = "image.html")]
+pub struct ImageTemplate<'a> {
+    pub gallery: &'a Gallery,
+    pub gallery_opts: &'a GalleryOpts,
+
+    pub image: &'a Image,
+    pub previous_image: Option<&'a Image>,
+    pub next_image: Option<&'a Image>,
+
+    pub index: u32,
 }
